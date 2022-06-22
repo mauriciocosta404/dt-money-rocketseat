1d3a2c659b7e0b9a4c8983635f78a1a7359522b1
menezescosta <mc611641@gmail.com>
2022-06-22 01:40:06 +0100

    api construida

--- src/components/transactionsTable/index.tsx
+++ src/components/transactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import * as C from './style';
+import { api } from '../../services/api';
 
 const TransactionsTable:React.FC=()=>{
+
+    useEffect(()=>{
+        api.get('https://localhost:3000/api/transactions').
+        then((response)=>console.log(response.data));
+    },[]);
+
     return(
         <C.Container>
             <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createServer} from 'miragejs';
 import App from './App';
+
+createServer({
+  routes(){
+    this.namespace='api';
+    this.get('/transactions',()=>{
+      return [
+        {
+          id:1,
+          title:'transactions',
+          amount:400,
+          type:'deposit',
+          category:'food',
+          createAt:new Date()
+        }
+      ]
+    });
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
